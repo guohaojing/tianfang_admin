120af2b48acd7a42e82447c5706b1fd76f0d4531
guohaojing <13663677607@qq.com>
2017-08-01 17:39:22 +0800

    8.1

--- src/App.js
+++ src/App.js
@@ -1,68 +1,77 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 import logo from './logo.svg';
-import Cases from './js/news';
-import Cases2 from './js/advantage';
+import Cases from './js/News';
+import Cases2 from './js/Advantage';
+import Vi from './js/Vi';
 import './css/admin.css';
 import './css/pintuer.css';
 import './css/css.css';
+import './css/vi.css';
 import './css/news.css';
 class App extends Component {
 	    componentDidMount=function () {
 	  $(".leftnav h2").click(function(){
-	  $(this).next().slideToggle(200);	
-	  $(this).toggleClass("on"); 
-  })
+  	  $(this).next().slideToggle(200);	
+  	  $(this).toggleClass("on"); 
+    })
   $(".leftnav ul li a").click(function(){
-	    $("#a_leader_txt").text($(this).text());
-  		$(".leftnav ul li a").removeClass("on");
-		$(this).addClass("on");
-  })
+  	    $("#a_leader_txt").text($(this).text());
+    		$(".leftnav ul li a").removeClass("on");
+  		$(this).addClass("on");
+    })
     $("#cases").click(function(){
-    	$(".admin").children().hide()
-    	$(".cases").show()
-    	$(".bread li:nth-child(2)").text("ADVANTAGE优势")
-  })
-        $("#cases2").click(function(){
-    	$(".admin").children().hide()
-    	$(".cases2").show()
-    	$(".bread li:nth-child(2)").text("NEWS资讯")
-  })
+      	$(".admin").children().hide()
+      	$(".cases").show()
+      	$(".bread li:nth-child(2)").text("ADVANTAGE优势")
+    })
+      $("#cases2").click(function(){
+        	$(".admin").children().hide()
+        	$(".cases2").show()
+        	$(".bread li:nth-child(2)").text("NEWS资讯")
+      })
+      $("#vi").click(function(){
+          $(".admin").children().hide()
+          $(".vi").show()
+          $(".bread li:nth-child(2)").text("标志+VI")
+      })
 	    }
   render() {
     return (
       <div className="App">
-    	<div className="header bg-main">
-  <div className="logo margin-big-left fadein-top">
-    <h1>后台管理中心</h1>
-  </div>
-</div>
-<div className="leftnav">
-  <div className="leftnav-title"><strong><span className="icon-list"></span>菜单列表</strong></div>
-  <h2><span className="icon-user"></span>基本设置</h2>
-  <ul>
-    <li><a><span className="icon-caret-right"></span>网站设置</a></li>
-    <li><a><span className="icon-caret-right"></span>单页管理</a></li>
-    <li><a><span className="icon-caret-right"></span>栏目管理</a></li>
-  </ul>   
-  <h2><a><span className="icon-pencil-square-o"></span>栏目管理</a></h2>
-  <ul>
-    <li><a><span className="icon-caret-right"></span>内容管理</a></li>
-    <li><a><span className="icon-caret-right"></span>添加内容</a></li>
-    <li><a><span className="icon-caret-right"></span>分类管理</a></li> 
-    <li id="cases"><a><span className="icon-caret-right"></span>NEWS资讯</a></li>   
-    <li id="cases2"><a><span className="icon-caret-right"></span>ADVANTAGE优势</a></li>   
-  </ul>  
-</div>
-<ul className="bread">
-  <li> 首页</li>
-  <li>网站信息</li>
-</ul>
-<div className="admin">
-<div className="cases"><Cases></Cases></div>
-<div className="cases2"><Cases2></Cases2></div>
-<p>后台管理系统</p>
-</div>
+          <div className="header bg-main">
+            <div className="logo margin-big-left fadein-top">
+              <h1>后台管理中心</h1>
+            </div>
+          </div>
+          <div className="leftnav">
+            <div className="leftnav-title"><strong><span className="icon-list"></span>菜单列表</strong></div>
+            <h2><span className="icon-user"></span>基本设置</h2>
+            <ul>
+              <li><a><span className="icon-caret-right"></span>网站设置</a></li>
+              <li><a><span className="icon-caret-right"></span>单页管理</a></li>
+              <li><a><span className="icon-caret-right"></span>栏目管理</a></li>
+            </ul>   
+            <h2><a><span className="icon-pencil-square-o"></span>栏目管理</a></h2>
+            <ul>
+              <li><a><span className="icon-caret-right"></span>内容管理</a></li>
+              <li><a><span className="icon-caret-right"></span>添加内容</a></li>
+              <li><a><span className="icon-caret-right"></span>分类管理</a></li> 
+              <li id="cases"><a><span className="icon-caret-right"></span>NEWS资讯</a></li>   
+              <li id="cases2"><a><span className="icon-caret-right"></span>ADVANTAGE优势</a></li>
+              <li id="vi"><a><span className="icon-caret-right"></span>标志+VI</a></li> 
+            </ul>  
+          </div>
+          <ul className="bread">
+            <li> 首页</li>
+            <li>网站信息</li>
+          </ul>
+          <div className="admin">
+            <div className="cases"><Cases></Cases></div>
+            <div className="cases2"><Cases2></Cases2></div>
+            <div className="vi"><Vi></Vi></div>
+            <p>后台管理系统</p>
+          </div>
       </div>
     );
   }
